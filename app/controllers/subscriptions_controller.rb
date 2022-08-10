db2acff0d6fcdccd139f2038c9e767e661b78545
Nicolas Arias-Gonzalez <nariasgonzalez@aha.io>
2022-08-10 08:32:56 -0500

    Add unfollow action

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,7 @@
 class SubscriptionsController < ApplicationController
   include Internationalized
   around_action :switch_locale
+  before_action :set_event
 
   skip_before_action :verify_authenticity_token
 
@@ -8,24 +9,31 @@ class SubscriptionsController < ApplicationController
   rescue_from Errors::TeamNotFound, with: :render_not_found
 
   def create
-    all_params = subscription_params
-
-    search_team = all_params.delete(:team)
-    Rails.logger.info("Searching events matching #{search_team}")
-
-    event = EventManager.find_matching search_team
-
-    raise Errors::EventNotFound, I18n.t(:match_not_found) unless event
-
-    subscription = event.subscriptions.find_or_initialize_by(all_params)
+    subscription = @event.subscriptions.find_or_initialize_by(subscription_params)
 
     render json: { message: "#{I18n.t :following_match} #{subscription.event.title}" } if subscription.save
   end
 
+  def destroy
+    subscription = @event.subscriptions.find_by!(subscription_params)
+
+    render json: { message: "#{I18n.t :unfollowed_match} #{@event.title}" } if subscription.destroy
+  rescue ActiveRecord::RecordNotFound
+    render json: { message: "#{I18n.t :subscription_not_found}" }
+  end
+
   private
 
+  def set_event
+    search_team = params[:subscription][:team]
+    Rails.logger.info("Searching events matching #{search_team}")
+
+    @event = EventManager.find_matching search_team
+    raise Errors::EventNotFound, I18n.t(:match_not_found) unless @event
+  end
+
   def subscription_params
-    params.require(:subscription).permit(:team, :service, :conversation_id)
+    params.require(:subscription).permit(:service, :conversation_id)
   end
 
   def render_not_found(err)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /en|es/ do
     resources :team_aliases, only: [:create]
     resources :subscriptions, only: [:create]
+    delete 'subscriptions', to: 'subscriptions#destroy'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
